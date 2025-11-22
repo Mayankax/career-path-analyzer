2a606dcde919df57d2abb9ae339b0758a7a664ca
Mayankax <mayankbansal818@gmail.com>
2025-11-22 19:16:14 +0530

    Prepare frontend for deployment

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // backend URL
+  baseURL: "https://career-path-backend-uped.onrender.com", // backend URL
 });
 
 export const getSkillGap = (payload: any) =>
